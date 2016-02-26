e6be488901beff9611103350e98d24606d3ebc14
Richard Rollins <rprollins@users.noreply.github.com>
2016-02-26 16:29:35 +0000

    Walls benchmark code cleanup

--- benchmarks/Benchmark_walls.cc
+++ benchmarks/Benchmark_walls.cc
@@ -28,82 +28,83 @@ Author: Richard Rollins <rprollins@users.noreply.github.com>
 
 #include <Grid.h>
 
+// Class for the dynamical evolution of cosmological domain walls
+
 class Walls
 {
 
 public:
 
+  // Vectorized scalar and vectorized scalar field data types
   typedef Grid::iScalar<Grid::vRealD> vScalar;
   typedef Grid::Lattice<vScalar>      vScalarField;
-  typedef vScalarField *              pvScalarField;
-  typedef vScalarField const *        pcvScalarField;
 
-  Walls(Grid::GridCartesian * Grid, vScalarField * P1, vScalarField * P2) :
+  // Real value data type
+  typedef Grid::RealD RealType;
+
+  Walls(Grid::GridCartesian * Grid, vScalarField * phi1, vScalarField * phi2) :
 
     stencil(Grid,npoint,Even,directions,displacements),
-    dct(tampasso * ctinitial),
-    expoentec(expoente / (1.0 - expoente)),
-    dx2r(1.0 / (ctinitial * ctinitial)),
-    omega0(0.5 * W0 * ctinitial),
+    dnu(dlnnu * nu0),
+    iz(a0 / (1.0 - a0)),
+    omega0(0.5 * W0 * nu0),
     V0(0.5 * M_PI * M_PI / ( omega0 * omega0)),
-    idct(1.0 / dct),
-    qmiu(0.0),
-    vdx2r(dx2r),
-    vdct(dct),
-    vidct(idct),
-    vqmiu(qmiu)
+    vidx2(1.0 / (nu0 * nu0)),
+    vdnu(dnu),
+    vidnu(1.0 / dnu),
+    vqmiu(0.0)
 
   {
     // Initialize pointers to scalar fields
-    Pold = P2;
-    Pnow = P1;
-    Pnew = P2;
+    phi_old = phi2;
+    phi_now = phi1;
+    phi_new = phi2;
 
     // Initialize time varaibles
-    ct_prev = ctinitial;
-    ct      = ctinitial;
+    nu_old = nu0;
+    nu_now = nu0;
   }
 
   void timestep()
   {
     // Calculate global varaibles
-    delta      = 0.5 * alfa * expoentec * dct / ct;
-    i1pdelta   = 1.0 / (1.0 + delta);
-    m1delta    = 1.0 - delta;
-    V04atbeta0 = 4.0 * V0 * pow( pow(ct_prev,expoentec), beta0 );
-
-    vScalar vm1delta    = m1delta;
-    vScalar vV04atbeta0 = V04atbeta0;
-    vScalar vi1pdelta   = i1pdelta;
+    delta      = 0.5 * alpha * iz * dnu / nu_now;
+    v1mdelta   = 1.0 - delta;
+    v4V0atbeta = 4.0 * V0 * pow( pow(nu_old,iz), beta );
+    vi1pdelta  = 1.0 / (1.0 + delta);
 
     // Iterate over all grid sites
     PARALLEL_FOR_LOOP
-    for(int i=0; i<Pnow->_grid->oSites(); i++)
+    for(int i=0; i<phi_now->_grid->oSites(); i++)
     {
-      // Laplacian
-      vScalar Lphi = Laplacian(i);
-      // Current time derivative
-      vScalar DPnow_ijkl = (Pnow->_odata[i] - Pold->_odata[i]) * vidct;
-      // New time derivative
-      vScalar DPnew_ijkl = (vm1delta * DPnow_ijkl + vdct * (Lphi - vV04atbeta0 * ((Pnow->_odata[i]*(Pnow->_odata[i]*Pnow->_odata[i]-1.0)) + vqmiu))) * vi1pdelta;
-      // Dynamical evolution of P
-      Pnew->_odata[i] = Pnow->_odata[i] + vdct * DPnew_ijkl;
+      // Laplacian as grid site i
+      Lphi = Laplacian(i);
+      // Current time derivative at i
+      dphi_now = (phi_now->_odata[i] - phi_old->_odata[i]) * vidnu;
+      // Potential gradient at i
+      dVdphi = v4V0atbeta * ( phi_now->_odata[i]*( phi_now->_odata[i]*phi_now->_odata[i] - 1.0 ) + vqmiu );
+      // New time derivative at i
+      dphi_new = ( v1mdelta*dphi_now + vdnu*( Lphi - dVdphi ) ) * vi1pdelta;
+      // Dynamical evolution of phi at i
+      phi_new->_odata[i] = phi_now->_odata[i] + vdnu * dphi_new;
     }
 
     // Halo exchange
-    stencil.HaloExchange(*Pnew,compressor);
+    stencil.HaloExchange(*phi_new,compressor);
 
     // Increment time
-    ct_prev  = ct;
-    ct      += dct;
+    nu_old  = nu_now;
+    nu_now += dnu;
 
     // Switch array aliases
-    Pold = const_cast<vScalarField const *>(Pnow);
-    Pnow = const_cast<vScalarField const *>(Pnew);
-    Pnew = const_cast<vScalarField *>(Pold);
+    phi_old = const_cast<vScalarField const *>(phi_now);
+    phi_now = const_cast<vScalarField const *>(phi_new);
+    phi_new = const_cast<vScalarField *>(phi_old);
   }
 
 private:
+
+  // Stencil variables
 
   const int ndim   = 3;
   const int npoint = 6;
@@ -114,40 +115,40 @@ private:
   Grid::CartesianStencil<vScalar,vScalar> stencil;
   Grid::SimpleCompressor<vScalar> compressor;
 
-  const Grid::Real alfa       = 3.0;
-  const Grid::Real beta0      = 0.0;
-  const Grid::Real W0         = 10.0;
+  // Simulation constants and variables
 
-  const Grid::Real expoente   = 0.6666666;
-  const Grid::Real ctinitial  = 1.0;
-  const Grid::Real tampasso   = 0.25;
+  const RealType alpha = 3.0;       // Constant alpha
+  const RealType beta  = 0.0;       // Constant beta
+  const RealType W0    = 10.0;      // Ratio of wall thickness to horizon size at nu0
+  const RealType nu0   = 1.0;       // Initial conformal time
+  const RealType a0    = 0.6666666; // Scale factor at nu0
+  const RealType dlnnu = 0.25;      // Delta (ln conformal time) per timestep
 
-  const Grid::Real dct;
-  const Grid::Real expoentec;
-  const Grid::Real dx2r;
-  Grid::Real ct;
-  Grid::Real ct_prev;
-  const Grid::Real omega0;
-  const Grid::Real V0;
+  const RealType iz;     // Inverse redshift
+  const RealType omega0; // Wall thickness
+  const RealType V0;     // Potential barrier
+  RealType nu_now;       // Current conformal time
+  RealType nu_old;       // Previous conformal time
 
-  Grid::Real delta;
-  const Grid::Real idct;
-  Grid::Real m1delta;
-  Grid::Real V04atbeta0;
-  const Grid::Real qmiu;
-  Grid::Real i1pdelta;
+  const RealType dnu;   // Conformal timestep size
+  const vScalar  vdnu;  // Vector conformal timestep size
+  const vScalar  vidnu; // Vector inverse conformal timestep size
+  const vScalar  vidx2; // Vector inverse of the squared grid size
+  const vScalar  vqmiu; // Vector constant term in potential gradient
 
-  const vScalar vdct;
-  const vScalar vidct;
-  vScalar vm1delta;
-  vScalar vV04atbeta0;
-  const vScalar vqmiu;
-  vScalar vi1pdelta;
-  const vScalar vdx2r;
+  RealType delta;      // Repeated term in dynamical evolution equation
+  vScalar  v1mdelta;   // Vector 1 - delta
+  vScalar  vi1pdelta;  // Vector 1 / ( 1 + delta )
+  vScalar  v4V0atbeta; // Vector potential gradient factor
 
-  vScalarField const * Pold;
-  vScalarField const * Pnow;
-  vScalarField *       Pnew;
+  vScalar Lphi;     // Laplacian of phi
+  vScalar dphi_now; // Current time derivative of phi
+  vScalar dphi_new; // New time derivative of phi
+  vScalar dVdphi;   // Potential gradient
+
+  vScalarField const * phi_old; // Scalar field phi at previous timestep
+  vScalarField const * phi_now; // Scalar field phi at current timestep
+  vScalarField *       phi_new; // Write location for phi at next timestep
 
   inline vScalar Laplacian(int i)
   {
@@ -166,9 +167,9 @@ private:
     {
       SE = stencil.GetEntry(permute_type,j,i);
       if ( SE->_is_local && SE->_permute )
-        permute(SV,Pnow->_odata[SE->_offset],permute_type);
+        permute(SV,phi_now->_odata[SE->_offset],permute_type);
       else if (SE->_is_local)
-        SV = Pnow->_odata[SE->_offset];
+        SV = phi_now->_odata[SE->_offset];
       else
         SV = stencil.comm_buf[SE->_offset];
       if(j==0) {Lphi  = SV;}
@@ -176,8 +177,8 @@ private:
     }
 
     // Complete Laplacian
-    Lphi -= 6.0 * Pnow->_odata[i];
-    return Lphi * vdx2r;
+    Lphi -= 6.0 * phi_now->_odata[i];
+    return Lphi * vidx2;
   }
 };
 
@@ -204,16 +205,16 @@ int main (int argc, char ** argv)
   Grid::GridParallelRNG pRNG(&Grid);
   pRNG.SeedRandomDevice();
 
-  // Scalar fields P1 and P2 are initialized to the same sample
+  // Scalar fields phi1 and phi2 are initialized to the same sample
   // from the uniform random field [-1.0,1.0] on the Grid
-  Walls::vScalarField P1(&Grid);
-  Walls::vScalarField P2(&Grid);
-  random(pRNG,P1);
-  P1 = (2.*P1) - 1.;
-  P2 = P1;
+  Walls::vScalarField phi1(&Grid);
+  Walls::vScalarField phi2(&Grid);
+  random(pRNG,phi1);
+  phi1 = (2.*phi1) - 1.;
+  phi2 = phi1;
 
   // Walls object
-  Walls walls(&Grid,&P1,&P2);
+  Walls walls(&Grid,&phi1,&phi2);
 
   // Start timer
   double start = Grid::usecond();
@@ -226,7 +227,7 @@ int main (int argc, char ** argv)
   double time = (stop-start) * 1.0E-6;
 
   // Memory throughput and Flop rate
-  double bytes = vol * sizeof(Grid::Real) * 2 * Nloop;
+  double bytes = vol * sizeof(Walls::RealType) * 2 * Nloop;
   double flops = double(vol) * double(22) * double(Nloop);
   std::cout << Grid::GridLogMessage << std::setprecision(5)
             << "GB/s: "    << 1.0E-9 * bytes / time << "\t"
